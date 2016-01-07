5266f7e0318ea12b3244b2110c1214ce4ed924ba
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-01-07 10:01:32 +0200

    Add travis config & code coverage scripts & code refactoring

--- karma.conf.js
+++ karma.conf.js
@@ -1,54 +1,28 @@
 module.exports = function (config) {
     config.set({
-
-
         basePath: '',
-
-
         frameworks: ['jasmine'],
-
-
         files: [
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
             'lib/*.js',
             'tests/*.js'
         ],
-
-
         coverageReporter: {
-            type: 'html',
-            dir: 'coverage/'
+            type: 'lcov',
+            dir: 'coverage',
+            subdir: '.'
         },
-
-
         exclude: [],
-
-
         preprocessors: {
-
             'lib/*.js': ['coverage']
         },
-
-
         reporters: ['progress', 'coverage'],
-
-
         port: 9876,
-
-
         colors: true,
-
-
         logLevel: config.LOG_INFO,
-
-
         autoWatch: true,
-
-
-        browsers: ['Chrome'],
-
-
+        browsers: ['PhantomJS'],
         singleRun: false
     });
 };
